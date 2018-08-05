f1ffe42daa451c047413d2ce49ed91c87574a002
kemchik <kerminsk@gmail.com>
2018-08-05 16:47:38 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict'
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './app/app.jsx',
+    output:{
+        filename: "bundle.js"       // название создаваемого файла
+    },
+    module:{
+        rules:[   //загрузчик для jsx
+            {
+                test: /\.jsx?$/, // определяем тип файлов
+                exclude: /(node_modules)/,  // исключаем из обработки папку node_modules
+                loader: "babel-loader",   // определяем загрузчик
+                options:{
+                    presets:["env", "react"]    // используемые плагины
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+}
