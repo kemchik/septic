867985dddfd7d083622b54e04c7a4519fcfe435e
kemchik <kerminsk@gmail.com>
2018-08-13 12:19:24 +0300

    make public

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict'
 const email = require('./moduls/email.js');
+const express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(8080);
 
 email('Kira', 'Phone');
