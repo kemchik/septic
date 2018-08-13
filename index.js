3f21b8cc058e2b3dce1c9bf6df515cba258fc0ad
kemchik <kerminsk@gmail.com>
2018-08-13 18:54:28 +0300

    make on node

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 'use strict'
 const email = require('./moduls/email.js');
-const express = require('express'),
-    app = express();
+const path = require('path');
+const express = require('express');
+var bodyParser = require("body-parser");
+const exphbs = require('express-handlebars');
+const app = express();
 
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
 app.use(express.static(__dirname + '/public'));
-app.listen(8080);
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'));
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.post("/", urlencodedParser, function (request, response) {
+    if(!request.body) {
+    } else {
+        email(request.body.userName, request.body.userPhoneNumber);
+        response.render('home', {})
+    }
+
+});
+
+app.get('/', (request, response) => {
+    response.render('home', {})
+});
+
+app.listen(3000);
+
 
 email('Kira', 'Phone');
